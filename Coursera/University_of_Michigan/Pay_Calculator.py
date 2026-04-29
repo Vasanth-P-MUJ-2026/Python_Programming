2afe0401d3f5d24a0d6db144e34d9174bb2984e4
Vasanth P <i.vasanth.prakasam@gmail.com>
2026-04-29 12:45:50 +0530

    Add Pay Calculator for computing gross pay

---
+++ Coursera/University_of_Michigan/Pay_Calculator.py
@@ -0,0 +1,9 @@
+# for the user input & Type casting
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+#  Compute gross pay
+gross_pay = hours * rate
+
+#display the result
+print("Pay:", gross_pay)
