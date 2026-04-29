90768f71378540e75aa2412e1d42eb2ce0bb86ed
Vasanth P <i.vasanth.prakasam@gmail.com>
2026-04-29 13:06:09 +0530

    Add second method for calculating pay
    for input
    hours = 35
    rate = 2.75

--- Coursera/University_of_Michigan/Pay_Calculator.py
+++ Coursera/University_of_Michigan/Pay_Calculator.py
@@ -1,3 +1,4 @@
+# Method-1
 # for the user input & Type casting
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
@@ -7,3 +8,9 @@ gross_pay = hours * rate
 
 #display the result
 print("Pay:", gross_pay)
+
+# Method-2
+xh = input("Enter hour: ")
+xr = input("Enter rate: ")
+xp = float(xh) * float(xr)
+print("Pay:", xp)
